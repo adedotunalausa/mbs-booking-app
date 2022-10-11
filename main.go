51a470bdc98a1a2a2b1fcf186d64dd8f13d9fc61
adedotunalausa <adedotunalausa@gmail.com>
2022-10-11 20:40:22 +0100

    add input validation and better error message

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "MBS Conference"
@@ -14,31 +17,51 @@ func main() {
 
 	for {
 		var firstName string
+		var email string
 		var ticketsOrdered uint
 		fmt.Println("Enter your name: ")
 		fmt.Scan(&firstName)
+		fmt.Println("Enter your email")
+		fmt.Scan(&email)
 		fmt.Println("Enter the number of tickets to buy: ")
 		fmt.Scan(&ticketsOrdered)
 
-		if ticketsOrdered > remainingTickets {
-			fmt.Printf("%v, We only have %v tickets left. So you can't book %v tickets\n",
-				firstName, remainingTickets, ticketsOrdered)
+		isValidName := len(firstName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketRequest := ticketsOrdered <= remainingTickets
+		isValidTicketInput := ticketsOrdered > 0
+
+		if isValidName && isValidEmail && isValidTicketRequest && isValidTicketInput {
+			remainingTickets = remainingTickets - ticketsOrdered
+			bookings = append(bookings, firstName+",")
+
+			fmt.Printf("First item in bookings is: %v. \n", bookings[0])
+			fmt.Printf("Bookings is of type: %T. \n", bookings)
+			fmt.Printf("We currently have %v booking(s). \n", len(bookings))
+
+			fmt.Printf("Thank you %v for buying %v tickets \n", firstName, ticketsOrdered)
+			fmt.Printf("We have %v tickets left \n", remainingTickets)
+			fmt.Printf("The bookings are: %v. \n", bookings)
+		} else {
+			if !isValidName {
+				fmt.Printf("%v is not a valid name\n", firstName)
+			}
+			if !isValidEmail {
+				fmt.Printf("%v is not is not a valid email\n", email)
+			}
+			if !isValidTicketRequest {
+				fmt.Printf("%v, We only have %v tickets left. So you can't book %v tickets\n",
+					firstName, remainingTickets, ticketsOrdered)
+			}
+			if !isValidTicketInput {
+				fmt.Printf("Tickets ordered should be greater than zero and not %v\n", ticketsOrdered)
+			}
 			continue
 		}
 
-		remainingTickets = remainingTickets - ticketsOrdered
-		bookings = append(bookings, firstName+",")
-
-		fmt.Printf("First item in bookings is: %v. \n", bookings[0])
-		fmt.Printf("Bookings is of type: %T. \n", bookings)
-		fmt.Printf("We currently have %v booking(s). \n", len(bookings))
-
-		fmt.Printf("Thank you %v for buying %v tickets \n", firstName, ticketsOrdered)
-		fmt.Printf("We have %v tickets left \n", remainingTickets)
-		fmt.Printf("The bookings are: %v. \n", bookings)
-
 		if remainingTickets == 0 {
-			fmt.Printf("")
+			fmt.Printf("***** All tickets are booked, thank you! *****\n")
+			break
 		}
 	}
 }
